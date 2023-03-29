456d70e80541c549864c783f8aa8dc049fb7f926
Alex A. Peraza <2020152022@ub.edu.bz>
2023-03-29 12:59:51 -0600

    greeting handler

--- test-1.go
+++ test-1.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
-	
-	
+	"strings"
+	"time"
 )
 
 // bioHandler handles the root route
@@ -38,3 +38,60 @@ func bioHandler(w http.ResponseWriter, r *http.Request) {
 	`
 	fmt.Fprint(w, html)
 } 
+
+// getGreeting returns a greeting based on the time of day
+func getGreeting(t time.Time) string {
+	switch t.Weekday() {
+	case time.Monday:
+		return "Monday!"
+	case time.Tuesday:
+		return "Tuesday!"
+	case time.Wednesday:
+		return "Wednesday!"
+	case time.Thursday:
+		return "Thursday!"
+	case time.Friday:
+		return "Friday!"
+	case time.Saturday:
+		return "Saturday!"
+	case time.Sunday:
+		return "Sunday!"
+	default:
+		return ""
+	}
+}
+
+// greetingHandler handles the /greeting route
+func greetingHandler(w http.ResponseWriter, r *http.Request) {
+	now := time.Now()
+	greeting := getGreeting(now)
+	html := `
+		<!DOCTYPE html>
+		<html>
+			<head>
+				<title>Greeting</title>
+				<style>
+					body {
+						font-family: Arial, sans-serif;
+						font-size: 16px;
+						line-height: 1.5;
+						background-color: #f7f7f7;
+						text-align: center;
+						padding: 2em;
+					}
+					h1 {
+						font-size: 2.5em;
+						margin-bottom: 1em;
+					}
+				</style>
+			</head>
+			<body>
+				<h1>Right now is {{time}}</h1>
+				<p>Enjoy the rest of your {{greeting}}.</p>
+			</body>
+		</html>
+	`
+	html = strings.ReplaceAll(html, "{{time}}", now.Format("3:04pm"))
+	html = strings.ReplaceAll(html, "{{greeting}}", greeting)
+	fmt.Fprint(w, html)
+}
